d0e563b221204f591304730581f018c326746d1b
jeredom <jeremiah@interact.io>
2018-11-15 14:24:03 +0100

    Methoden für beschleunigen und abbremsen

--- src/reissverschlussverfahren/MyAgentContextBuilder.java
+++ src/reissverschlussverfahren/MyAgentContextBuilder.java
@@ -10,24 +10,19 @@ import repast.simphony.parameter.Parameters;
 import repast.simphony.space.continuous.RandomCartesianAdder;
 import repast.simphony.space.continuous.ContinuousSpace;
 
-public class MyAgentContextBuilder implements ContextBuilder<IMyAgent> {
+public class MyAgentContextBuilder implements ContextBuilder<Auto> {
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public Context<IMyAgent> build(Context<IMyAgent> context) {
-		// The x dimension of the physical space
+	public Context<Auto> build(Context<Auto> context) {
 		int xdim = 20;
-		// The y dimension of the physical space
 		int ydim = 20;
-		// Create a new 2D continuous space to model the physical space
+		
 		ContinuousSpace<Object> continuousSpace = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null).createContinuousSpace("ContinuousSpace2D",
 				context, new RandomCartesianAdder(), new repast.simphony.space.continuous.StickyBorders(), xdim, ydim);
 		NetworkFactoryFinder.createNetworkFactory(null).createNetwork("SensorNetwork", context, false);
-		// The environment parameters contain the user-editable values that appear in
-		// the GUI.
-		// Get the parameters p and then specifically the initial numbers of wolves and
-		// sheep.
 		Parameters p = RunEnvironment.getInstance().getParameters();
+		
 		for (String s : p.getSchema().parameterNames()) {
 			System.out.println(s + "; ");
 		}
@@ -38,7 +33,7 @@ public class MyAgentContextBuilder implements ContextBuilder<IMyAgent> {
 		return context;
 	}
 	
-	private void addAutosToContinuousSpace(Context<IMyAgent> context, ContinuousSpace<Object> continuousSpace) {
+	private void addAutosToContinuousSpace(Context<Auto> context, ContinuousSpace<Object> continuousSpace) {
 		Auto auto = new Auto(continuousSpace, 50d, 30d, 30d);
 		context.add(auto);
 	}
--- src/reissverschlussverfahren/model/Auto.java
+++ src/reissverschlussverfahren/model/Auto.java
@@ -1,14 +1,14 @@
 package reissverschlussverfahren.model;
 
-import reissverschlussverfahren.IMyAgent;
 import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.engine.schedule.ScheduleParameters;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.NdPoint;
 
-public class Auto implements IMyAgent{
+public class Auto {
 	
 	public final Double höchstgeschwindigkeit;
+	public Double aktuelleGeschwindigkeit = 0d;
 	public final Double maxPositiveBeschleunigung;
 	public final Double maxNegativeBeschleunigung;
 	private ContinuousSpace<Object> continuousSpace;
@@ -23,26 +23,22 @@ public class Auto implements IMyAgent{
 		RunEnvironment.getInstance().getCurrentSchedule().schedule(sp, this, "step");
 	}
 	
-	@Override
 	public void step() {
-		
+		accelerate();
 	}
 	
-	public void moveTo(NdPoint ndPoint) {
-		if(continuousSpace.getLocation(this) != ndPoint) {
-			
-		}
+	public NdPoint accelerate() {
+			Double neuXAchsenLocation = continuousSpace.getLocation(this).getX() + aktuelleGeschwindigkeit + maxPositiveBeschleunigung;
+			NdPoint newLocation = new NdPoint(neuXAchsenLocation, 2d);
+			aktuelleGeschwindigkeit = aktuelleGeschwindigkeit + maxPositiveBeschleunigung;
+			return continuousSpace.moveByDisplacement(this, newLocation.getX());	
 	}
 
-	@Override
-	public void accelerate() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void abbremsen() {
-		// TODO Auto-generated method stub
+		Double neuXAchsenLocation = continuousSpace.getLocation(this).getX() + aktuelleGeschwindigkeit + maxNegativeBeschleunigung;
+		NdPoint newLocation = new NdPoint(neuXAchsenLocation, 2d);
+		aktuelleGeschwindigkeit = aktuelleGeschwindigkeit + maxNegativeBeschleunigung;
+		continuousSpace.moveByDisplacement(this, newLocation.getX());	
 		
 	}
 
