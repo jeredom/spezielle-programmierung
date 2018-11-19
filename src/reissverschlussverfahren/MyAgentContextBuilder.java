0e1993af5022daefc39d4f31792d5fdb282b12c9
Anas Shammaa <anas.shammaa@diconium.com>
2018-11-19 18:52:11 +0100

    Hindernis auf Fahrbahn

--- src/reissverschlussverfahren/MyAgentContextBuilder.java
+++ src/reissverschlussverfahren/MyAgentContextBuilder.java
@@ -16,33 +16,38 @@ public class MyAgentContextBuilder implements ContextBuilder<IMyAgent> {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public Context<IMyAgent> build(Context<IMyAgent> context) {
-		int xdim = 20;
-		int ydim = 20;
-		
-		ContinuousSpace<Object> continuousSpace = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null).createContinuousSpace("space",
-				context, new RandomCartesianAdder(), new repast.simphony.space.continuous.WrapAroundBorders(), xdim, ydim);
-		NetworkFactoryFinder.createNetworkFactory(null).createNetwork("SensorNetwork", context, false);
 		Parameters p = RunEnvironment.getInstance().getParameters();
-		
-		context.addProjection(continuousSpace);
-		
+
 		for (String s : p.getSchema().parameterNames()) {
 			System.out.println(s + "; ");
 		}
 
-		int width = p.getInteger("spacewidth");
-		int height = p.getInteger("spaceheight");
-		
+		int xdim = p.getInteger("spacewidth");
+		int ydim = p.getInteger("spaceheight");
+
+		ContinuousSpace<Object> continuousSpace = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null)
+				.createContinuousSpace("space", context, new RandomCartesianAdder(),
+						new repast.simphony.space.continuous.WrapAroundBorders(), xdim, ydim);
+		NetworkFactoryFinder.createNetworkFactory(null).createNetwork("SensorNetwork", context, false);
+
 		this.addAgentsToContinuousSpace(context, continuousSpace);
-		
+
 		return context;
 	}
-	
+
 	private void addAgentsToContinuousSpace(Context<IMyAgent> context, ContinuousSpace<Object> continuousSpace) {
-		Auto auto = new Auto(continuousSpace, 50d, 6d, -6d);
+		Auto auto = new Auto(continuousSpace, 100d, 6d, -6d);
+		Auto auto1 = new Auto(continuousSpace, 50d, 6d, -6d);
+
 		Hindernis hindernis = new Hindernis();
 		context.add(auto);
+		context.add(auto1);
+
+		continuousSpace.moveTo(auto, 0d, 1.5d);
+		continuousSpace.moveTo(auto1, 0d, 4.5d);
+
 		context.add(hindernis);
+		continuousSpace.moveTo(hindernis, 50.0d, 5d);
 	}
 
 }
--- src/reissverschlussverfahren/model/Auto.java
+++ src/reissverschlussverfahren/model/Auto.java
@@ -14,15 +14,16 @@ public class Auto implements IMyAgent {
 	public final Double maxNegativeBeschleunigung;
 	private ContinuousSpace<Object> continuousSpace;
 	
-	public Auto(ContinuousSpace<Object> continuousSpace, Double höchstgeschwindigkeit, Double maxPositiveBeschleunigung, Double maxNegativeBeschleunigung) {
+	public Auto(ContinuousSpace<Object> continuousSpace, Double hoechstgeschwindigkeit, Double maxPositiveBeschleunigung, Double maxNegativeBeschleunigung) {
 		this.continuousSpace = continuousSpace;
-		this.hoechstgeschwindigkeit = höchstgeschwindigkeit;
+		this.hoechstgeschwindigkeit = hoechstgeschwindigkeit;
 		this.maxPositiveBeschleunigung = maxPositiveBeschleunigung;
 		this.maxNegativeBeschleunigung = maxNegativeBeschleunigung;
 		
 		ScheduleParameters sp = ScheduleParameters.createRepeating(1, 1);
 		RunEnvironment.getInstance().getCurrentSchedule().schedule(sp, this, "step");
 	}
+	
 	
 	public void step() {
 		if(aktuelleGeschwindigkeit < hoechstgeschwindigkeit) {
@@ -37,20 +38,23 @@ public class Auto implements IMyAgent {
 		Double neuXAchsenLocation;
 		
 		if(differenz > maxPositiveBeschleunigung) {
-			neuXAchsenLocation = continuousSpace.getLocation(this).getX() + aktuelleGeschwindigkeit + maxPositiveBeschleunigung;
+			neuXAchsenLocation = continuousSpace.getLocation(this).getX() + (aktuelleGeschwindigkeit + maxPositiveBeschleunigung)/100;
 			aktuelleGeschwindigkeit = aktuelleGeschwindigkeit + maxPositiveBeschleunigung;
 		}else {
-			neuXAchsenLocation = continuousSpace.getLocation(this).getX() + aktuelleGeschwindigkeit + differenz;
+			neuXAchsenLocation = continuousSpace.getLocation(this).getX() + (aktuelleGeschwindigkeit + differenz)/100;
 			aktuelleGeschwindigkeit = hoechstgeschwindigkeit;
 		}
-			NdPoint newLocation = new NdPoint(neuXAchsenLocation, 2d);
-			continuousSpace.moveByDisplacement(this, newLocation.getX());	
+			NdPoint newLocation = new NdPoint(neuXAchsenLocation, continuousSpace.getLocation(this).getY());
+			continuousSpace.moveTo(this, newLocation.getX(), newLocation.getY());	
+			System.out.println("beschleunigen\n" + "Besch." + aktuelleGeschwindigkeit + "Pos." +  continuousSpace.getLocation(this).getX());
 	}
 	
 	public void fahren() {
-		Double neuXAchsenLocation = continuousSpace.getLocation(this).getX() + hoechstgeschwindigkeit;
-		NdPoint newLocation = new NdPoint(neuXAchsenLocation, 2d);
-		continuousSpace.moveByDisplacement(this, newLocation.getX());	
+		Double neuXAchsenLocation = continuousSpace.getLocation(this).getX() + (hoechstgeschwindigkeit/100);
+		NdPoint newLocation = new NdPoint(neuXAchsenLocation, continuousSpace.getLocation(this).getY());
+		continuousSpace.moveTo(this, newLocation.getX(), newLocation.getY());
+		System.out.println("fahren\n" + "Besch." + aktuelleGeschwindigkeit + "Pos." +  continuousSpace.getLocation(this).getX());
+
 	}
 
 	public void abbremsen() {
@@ -65,7 +69,7 @@ public class Auto implements IMyAgent {
 			aktuelleGeschwindigkeit = 0d;
 		}
 		NdPoint newLocation = new NdPoint(neuXAchsenLocation, 2d);
-		continuousSpace.moveByDisplacement(this, newLocation.getX());	
+		continuousSpace.moveTo(this, newLocation.getX(), newLocation.getY());	
 		
 	}
 
